8a7e733fff1cfd3b7dd189d6df13dff667cccaf7
Nuntuch <nuntuch13589@gmail.com>
2021-11-08 15:42:18 +0700

    create parrot check funtion

--- mavenproject_spring_boot/src/main/java/com/mycompany/mavenproject_spring_boot/Main.java
+++ mavenproject_spring_boot/src/main/java/com/mycompany/mavenproject_spring_boot/Main.java
@@ -13,4 +13,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Parrot");
     }
+    public static boolean parrot(){
+    
+    return true;
+    }
 }
